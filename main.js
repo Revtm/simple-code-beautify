94604d2b5640b134c9d68ce7f13a45f79342f8de
Revtm <kykylizaro98@gmail.com>
2024-07-02 11:44:23 +0700

    feature: add json beautify logic

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
-const path = require('node:path')
+const { app, BrowserWindow, ipcMain } = require('electron/main');
+const {screen} = require('electron');
+const path = require('node:path');
 
 function createWindow () {
+    const {width, height} = screen.getPrimaryDisplay().workAreaSize;
     const mainWindow = new BrowserWindow({
+        width: width,
+        height: height,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
-    })
+    });
 
-    ipcMain.on('set-title', (event, title) => {
-        const webContents = event.sender
-        const win = BrowserWindow.fromWebContents(webContents)
-        win.setTitle(title)
-    })
+    // ipcMain.on('set-json', (event, title) => {
+    //
+    // })
 
-    mainWindow.loadFile('index.html')
+    // mainWindow.webContents.openDevTools()
+    mainWindow.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
 
     app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
