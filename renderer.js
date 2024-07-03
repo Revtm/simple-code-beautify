3cafbdc7b5e59e3706843ee0c376d2d0fbf11000
Revtm <kykylizaro98@gmail.com>
2024-07-03 10:06:08 +0700

    feature: add repo info

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 const setButton = document.getElementById('beautify-btn');
 const originalInput = document.getElementById('original-json');
 const beautifiedJson = document.getElementById('beautified-json');
+const errMsg = document.getElementById('beautify-error-msg');
 
 setButton.addEventListener('click', () => {
     try{
@@ -12,7 +13,8 @@ setButton.addEventListener('click', () => {
         replacedToHTML = "<span>".concat(replacedToHTML).concat("<span/>");
         console.log(replacedToHTML);
         beautifiedJson.innerHTML = replacedToHTML;
+        errMsg.innerHTML = "";
     }catch (e){
-        console.log(e);
+        errMsg.innerHTML = e;
     }
-})
+});
